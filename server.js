3a904b691d169b0d734a14d455677aa0c77f97ea
wijeremy <jeremydavwilliams@gmail.com>
2021-10-13 16:35:03 -0500

    fixed some bugs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://localhost/budget', {
 });
 
 // routes
-app.use(require('../routes/api.js'));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
