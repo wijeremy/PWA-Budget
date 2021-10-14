009638aa363112a5af18f1921a949d8d3e3ddd9c
wijeremy <jeremydavwilliams@gmail.com>
2021-10-14 16:07:32 -0500

    trying this

--- server.js
+++ server.js
@@ -17,20 +17,21 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(
-  (process.env.MONGODB_URI,
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  }) ||
-    ('mongodb://localhost/budget',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    })
+  }
+  // ||
+  // ('mongodb://localhost/budget',
+  // {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   useCreateIndex: true,
+  //   useFindAndModify: false,
+  // })
 );
 
 // routes
